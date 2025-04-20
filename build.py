5efcb0ec40fb1cd65e25e683f4a92d58aa51a94e
Vincent Guo <152289662+guo-vincent@users.noreply.github.com>
2025-04-20 05:38:56 -0400

    Venv now properly updated with environment variable

--- build.py
+++ build.py
@@ -3,13 +3,19 @@
 import os
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 
 def main():
     # Define the build directory relative to the script's location
     repo_root = Path(__file__).resolve().parents[0]  # Always point to TetrisEngine/
     build_dir = repo_root / "build"
-    vcpkg_root = os.environ.get('VCPKG_ROOT', 'C:')
+
+    # Set VCPKG_ROOT based on the platform
+    if sys.platform == "win32":
+        vcpkg_root = os.environ.get('VCPKG_ROOT', 'C:\\path\\to\\vcpkg')
+    else:
+        vcpkg_root = os.environ.get('VCPKG_ROOT', str(Path.home() / 'vcpkg'))
 
     # Remove the build directory if it exists
     if build_dir.exists() and build_dir.is_dir():
@@ -26,7 +32,7 @@ def main():
         "-DCMAKE_BUILD_TYPE=Release",
         "-DENABLE_NN=ON",
         "-DBUILD_TESTS=ON",
-        f"-DCMAKE_TOOLCHAIN_FILE={vcpkg_root}/vcpkg/scripts/buildsystems/vcpkg.cmake"
+        f"-DCMAKE_TOOLCHAIN_FILE={vcpkg_root}/scripts/buildsystems/vcpkg.cmake"
     ]
 
     # Run the CMake command
